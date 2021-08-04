58748a6590e8a930f94faaeddafacc276cbc7c80
Roman Le Négrate <roman.lenegrate@gmail.com>
2021-08-04 21:59:17 +0200

    use Utils::Log

--- main.rb
+++ main.rb
@@ -10,10 +10,11 @@ module Cmds
     dir = OUT.join Time.now.utc.strftime '%Y%m%dT%H%M%SZ'
     dir.mkdir
 
+    log = Utils::Log.new
     objects.each do |obj|
       next if obj =~ /^org-/
       dest = dir.join("#{obj}.json")
-      $stderr.puts " INFO writing #{dest.relative_path_from OUT}"
+      log.info "writing to #{dest.relative_path_from OUT}"
       dest.open 'w' do |f|
         IO.popen ["bw", "list", obj], 'r' do |p|
           IO.copy_stream p, f
